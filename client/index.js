47668937dbc4c90bb82e70f88456f6c37c41eff0
cesmec <cesmec@users.noreply.github.com>
2020-10-24 19:27:29 +0200

    Add js type annotations for socket.io

--- client/index.js
+++ client/index.js
@@ -2,11 +2,13 @@
 const csInterface = new CSInterface();
 const loc = window.location.pathname;
 const dir = decodeURI(loc.substring(1, loc.lastIndexOf('/')));
+/** @type {import("socket.io")} */
 const io = require(dir + "/node_modules/socket.io/lib/index.js");
 const fs = require('fs');
 
 let port = 42300;
 const portFile = "port.txt";
+/** @type {import("socket.io").Server} */
 let server = undefined;
 
 function init() {
