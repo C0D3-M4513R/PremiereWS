2899f859046a2ab7e0691d57a1803b992f75282c
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2020-11-14 23:19:29 +0100

    Add another interface for further abstraction

--- host/events.ts
+++ host/events.ts
@@ -1,42 +1,50 @@
-type WSEvent = ZoomEvent | MoveEvent | RotateEvent | OpacityEvent | AudioLevelEvent | LumetriEvent;
+interface WSEvent{
+  name:string
+}
+interface AbsoluteEvent extends WSEvent{
+  level?:number
+}
+interface RelativeEvent extends WSEvent{
+  delta?:number
+}
+interface RegularEvent extends AbsoluteEvent,RelativeEvent{}
+interface ResetEvent extends WSEvent{
+  reset?:boolean
+}
 
-interface MoveEvent {
+interface MoveEvent extends ResetEvent{
   name: "move"
-  deltaX: number
-  deltaY: number
-  reset?: boolean // sets position back to 0.5, 0.5
+  deltaX: number//TODO: make this fit in with RelativeEvent
+  deltaY: number//      maybe have delta and level in Relative and Absolute be a typevariable and have delta be an array?
+  // reset?: boolean // sets position back to 0.5, 0.5
 }
 
-interface ZoomEvent {
+interface ZoomEvent extends RegularEvent{
   name: "zoom"
-  level?: number
-  delta?: number 
-  reset?: boolean // sets value back to 1.0
+  // level?: number
+  // delta?: number
 }
 
-interface RotateEvent {
+interface RotateEvent extends RegularEvent{
   name: "rotate"
-  level?: number // 0 to 360+
-  delta?: number 
-  reset?: boolean // sets level back to 0
+  // level?: number // 0 to 360+
+  // delta?: number
 }
 
-interface OpacityEvent {
+interface OpacityEvent extends RegularEvent {
   name: "opacity"
-  level?: number // between 0 and 100
-  delta?: number
-  reset?: boolean // sets level back 0
+  // level?: number // between 0 and 100
+  // delta?: number
 }
 
-interface AudioLevelEvent {
+interface AudioLevelEvent extends RelativeEvent {
   name: "audio"
-  delta: number
+  // delta: number
 }
 
-interface LumetriEvent {
+interface LumetriEvent extends RegularEvent {
   name: "lumetri"
   property: number
-  level?: number
-  delta?: number
-  reset?: boolean // sets value back to default value
+  // level?: number
+  // delta?: number
 }
