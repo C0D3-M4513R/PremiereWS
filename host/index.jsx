801bab332486ece709ec95fa1baa9fa1d2eea6b7
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2020-11-18 20:00:57 +0100

    Built a simple Delegator Function

--- host/index.jsx
+++ host/index.jsx
@@ -1,128 +1,119 @@
+// type WSEvent = ZoomEvent | MoveEvent | RotateEvent | OpacityEvent | AudioLevelEvent | LumetriEvent;
 /// <reference path="../typings/JavaScript.d.ts"/>
 /// <reference path="../typings/PlugPlugExternalObject.d.ts"/>
 /// <reference path="../typings/PremierePro.14.0.d.ts"/>
 /// <reference path="../typings/XMPScript.d.ts"/>
 /// <reference path="../typings/extendscript.d.ts"/>
 /// <reference path="../typings/global.d.ts"/>
+/// <reference path="events.ts"/>
+//Tell TypeScript, if something is of a particular type
+function castRelativeEvent(data) {
+    return "delta" in data;
+}
+function castAbsoluteEvent(data) {
+    return "level" in data;
+}
+function castResetEvent(data) {
+    return "reset" in data;
+}
+function castMoveEvent(data) {
+    return "move" === data.name;
+}
 var demo = {
     showMsg: function () {
         alert("Hello World!");
     },
-    setZoom: function (zoomLevel) {
-        setZoomOfCurrentClip(zoomLevel);
-        return true;
-    },
     receiveEvent: function (data) {
         switch (data.name) {
             case "move":
-                if (data.reset) {
-                    resetPositionOfCurrentClip();
+                if (castResetEvent(data) && data.reset) {
+                    modifyClip(new ModifyInfo(1, 0), function () {
+                        return [0.5, 0.5];
+                    });
                 }
-                else {
-                    moveCurrentClip(data.deltaX, data.deltaY);
+                else if (castMoveEvent(data)) {
+                    modifyClip(new ModifyInfo(1, 0), function (info) {
+                        var value = info.getValue();
+                        return [value[0] + data.deltaX, value[1] + data.deltaY];
+                    });
                 }
                 break;
             case "zoom":
-                if (data.reset) {
-                    setZoomOfCurrentClip(100);
+                if (castRelativeEvent(data) && data.delta) {
+                    modifyClip(new ModifyInfo(1, 1), function (info) {
+                        return info.getValue() + data.delta;
+                    });
                 }
-                else if (data.delta) {
-                    changeZoomOfCurrentClip(data.delta);
-                }
-                else if (data.level) {
-                    setZoomOfCurrentClip(data.level);
+                else if (castAbsoluteEvent(data) && data.level) {
+                    modifyClip(new ModifyInfo(1, 1), function () {
+                        return data.level;
+                    });
                 }
                 break;
             case "rotate":
-                if (data.reset) {
-                    setRotationOfCurrentClip(0);
+                if (castRelativeEvent(data) && data.delta) {
+                    modifyClip(new ModifyInfo(1, 4), function (info) {
+                        return info.getValue() + data.delta;
+                    });
                 }
-                else if (data.delta) {
-                    rotateCurrentClip(data.delta);
-                }
-                else if (data.level) {
-                    setRotationOfCurrentClip(data.level);
+                else if (castAbsoluteEvent(data) && data.level) {
+                    modifyClip(new ModifyInfo(1, 4), function () {
+                        return data.level;
+                    });
                 }
                 break;
             case "opacity":
-                if (data.reset) {
-                    setOpacityOfCurrentClip(100);
+                if (castRelativeEvent(data) && data.delta) {
+                    modifyClip(new ModifyInfo(0, 0), function (info) {
+                        return info.getValue() + data.delta;
+                    });
                 }
-                else if (data.delta) {
-                    changeOpacityOfCurrentClip(data.delta);
-                }
-                else if (data.level) {
-                    setOpacityOfCurrentClip(data.level);
+                else if (castAbsoluteEvent(data) && data.level) {
+                    modifyClip(new ModifyInfo(0, 0), function () {
+                        return data.level;
+                    });
                 }
                 break;
             case "audio":
-                changeAudioLevel(data.delta);
+                if (castRelativeEvent(data) && data.delta) {
+                    modifyClip(new ModifyInfo(0, 0), function (info) {
+                        return levelToDB(dbToLevel(parseFloat(info.getValue())) + data.delta);
+                    });
+                }
                 break;
             case "lumetri":
-                if (data.reset) {
-                    resetLumetriProperty(data.property);
-                }
-                else if (data.delta) {
-                    changeLumetriProperty(data.delta, data.property, true);
-                }
-                else if (data.level) {
-                    changeLumetriProperty(data.level, data.property, false);
-                }
+                // TODO (1): Implement
+                // TODO (2): Create enum-like structure for easier property selection
                 break;
         }
     }
 };
-function moveCurrentClip(deltaX, deltaY) {
-    var clipInfo = getFirstSelectedClip(true);
-    var positionInfo = clipInfo.clip.components[1].properties[0];
-    var _a = positionInfo.getValue(), positionX = _a[0], positionY = _a[1];
-    positionInfo.setValue([positionX + deltaX, positionY + deltaY], true);
+var ModifyInfo = /** @class */ (function () {
+    function ModifyInfo(component, property, videoClip, setInfoBool) {
+        if (videoClip === void 0) { videoClip = true; }
+        if (setInfoBool === void 0) { setInfoBool = true; }
+        this.component = component;
+        this.property = property;
+        this.videoClip = videoClip;
+        this.setInfoBool = setInfoBool;
+    }
+    return ModifyInfo;
+}());
+/**
+ * Modify the clip according
+ * @param setting Which clip should get modified?
+ * @param processor How should that clip get modified?
+ */
+function modifyClip(setting, processor) {
+    var clipInfo = getFirstSelectedClip(setting.videoClip);
+    var info = clipInfo.clip.components[setting.component].properties[setting.property];
+    info.setValue(processor(info), setting.setInfoBool);
 }
-function resetPositionOfCurrentClip() {
-    var clipInfo = getFirstSelectedClip(true);
-    var positionInfo = clipInfo.clip.components[1].properties[0];
-    positionInfo.setValue([0.5, 0.5], true);
+function dbToLevel(db) {
+    return 20 * Math.log(db) * Math.LOG10E + 15;
 }
-function setZoomOfCurrentClip(zoomLevel) {
-    var clipInfo = getFirstSelectedClip(true);
-    var scaleInfo = clipInfo.clip.components[1].properties[1];
-    scaleInfo.setValue(zoomLevel, true);
-}
-function changeZoomOfCurrentClip(delta) {
-    var clipInfo = getFirstSelectedClip(true);
-    var scaleInfo = clipInfo.clip.components[1].properties[1];
-    var current = scaleInfo.getValue();
-    scaleInfo.setValue(current + delta, true);
-}
-function setRotationOfCurrentClip(level) {
-    var clipInfo = getFirstSelectedClip(true);
-    var info = clipInfo.clip.components[1].properties[4];
-    info.setValue(level, true);
-}
-function rotateCurrentClip(delta) {
-    var clipInfo = getFirstSelectedClip(true);
-    var info = clipInfo.clip.components[1].properties[4];
-    var current = info.getValue();
-    info.setValue(current + delta, true);
-}
-function setOpacityOfCurrentClip(level) {
-    var clipInfo = getFirstSelectedClip(true);
-    var info = clipInfo.clip.components[0].properties[0];
-    info.setValue(level, true);
-}
-function changeOpacityOfCurrentClip(delta) {
-    var clipInfo = getFirstSelectedClip(true);
-    var info = clipInfo.clip.components[0].properties[0];
-    var current = info.getValue();
-    info.setValue(current + delta, true);
-}
-function changeAudioLevel(levelInDb) {
-    var clipInfo = getFirstSelectedClip(false);
-    var levelInfo = clipInfo.clip.components[0].properties[1];
-    var level = 20 * Math.log(parseFloat(levelInfo.getValue())) * Math.LOG10E + 15;
-    var newLevel = level + levelInDb;
-    var encodedLevel = Math.min(Math.pow(10, (newLevel - 15) / 20), 1.0);
-    levelInfo.setValue(encodedLevel, true);
+function levelToDB(level) {
+    return Math.min(Math.pow(10, (level - 15) / 20), 1.0);
 }
 function getFirstSelectedClip(videoClip) {
     var currentSequence = app.project.activeSequence;
@@ -140,51 +131,4 @@ function getFirstSelectedClip(videoClip) {
         }
     }
     return null;
-}
-/**
- * Changes the selected property of the current clip's first lumetri component.
- * @param data the relative or absolut value to set or add
- * @param propertyIndex the index of the property to change
- * @param relative true, if the change shall be relative
- */
-function changeLumetriProperty(data, propertyIndex, relative) {
-    var lumetri = getLumetriComponent();
-    if (lumetri) {
-        var property = lumetri.properties[propertyIndex];
-        if (relative) {
-            property.setValue(property.getValue() + data, true);
-        }
-        else {
-            property.setValue(data, true);
-        }
-    }
-}
-/**
- * Resets the selected property of the current clip's first lumetri component.
- * @param propertyIndex
- */
-function resetLumetriProperty(propertyIndex) {
-    // FIXME: Not all properties of lumetri are currently supported
-    var lumetri = getLumetriComponent();
-    if (lumetri) {
-        var property = lumetri.properties[propertyIndex];
-        if (propertyIndex == 24 || propertyIndex == 31 || propertyIndex == 36) {
-            property.setValue(100, true);
-        }
-        else {
-            property.setValue(0, true);
-        }
-    }
-}
-/**
- * Returns the lumetri component of the currently selected clip or undefined.
- */
-function getLumetriComponent() {
-    var clip = getFirstSelectedClip(true).clip;
-    for (var i = 2; i < clip.components.length; i++) {
-        if (clip.components[i].properties[2].displayName && clip.components[i].properties[2].displayName === "Einfache Korrektur") {
-            return clip.components[i];
-        }
-    }
-    return undefined;
 }
--- host/index.tsx
+++ host/index.tsx
@@ -4,132 +4,124 @@
 /// <reference path="../typings/XMPScript.d.ts"/>
 /// <reference path="../typings/extendscript.d.ts"/>
 /// <reference path="../typings/global.d.ts"/>
+/// <reference path="events.ts"/>
 
 declare interface Track {
   overwriteClip(clipProjectItem: ProjectItem, time: Time): void;
 }
 
+//Tell TypeScript, if something is of a particular type
+function castRelativeEvent(data:WSEvent): data is RelativeEvent{
+  return "delta" in data
+}
+function castAbsoluteEvent(data:WSEvent): data is AbsoluteEvent{
+  return "level" in data
+}
+function castResetEvent(data:WSEvent): data is ResetEvent{
+  return "reset" in data
+}
+function castMoveEvent(data:WSEvent): data is MoveEvent{
+  return "move"===data.name
+}
+
+
 var demo = {
   showMsg: function () {
     alert("Hello World!")
   },
-  setZoom: function (zoomLevel: number): boolean {
-    setZoomOfCurrentClip(zoomLevel);
-    return true;
-  },
   receiveEvent: function (data: WSEvent) {
     switch (data.name) {
       case "move":
-        if (data.reset) {
-          resetPositionOfCurrentClip();
-        } else {
-          moveCurrentClip(data.deltaX, data.deltaY)
+        if (castResetEvent(data)&&data.reset) {
+          modifyClip(new ModifyInfo(1,0),() => {
+            return [0.5,0.5]
+          })
+        } else if (castMoveEvent(data)){
+          modifyClip(new ModifyInfo(1,0),(info) => {
+            const value=info.getValue()
+            return [value[0]+data.deltaX,value[1]+data.deltaY]
+          })
         }
         break;
       case "zoom":
-        if (data.reset) {
-          setZoomOfCurrentClip(100)
-        } else if (data.delta) {
-          changeZoomOfCurrentClip(data.delta)
-        } else if (data.level) {
-          setZoomOfCurrentClip(data.level)
+        if (castRelativeEvent(data)&&data.delta) {
+          modifyClip(new ModifyInfo(1,1),(info) => {
+            return info.getValue()+data.delta
+          })
+        } else if (castAbsoluteEvent(data)&&data.level) {
+          modifyClip(new ModifyInfo(1,1),() => {
+            return data.level
+          })
         }
         break;
       case "rotate":
-        if (data.reset) {
-          setRotationOfCurrentClip(0)
-        } else if (data.delta) {
-          rotateCurrentClip(data.delta)
-        } else if (data.level) {
-          setRotationOfCurrentClip(data.level)
+        if (castRelativeEvent(data)&&data.delta) {
+          modifyClip(new ModifyInfo(1,4),(info) => {
+            return info.getValue()+data.delta
+          })
+        } else if (castAbsoluteEvent(data)&&data.level) {
+          modifyClip(new ModifyInfo(1,4),() => {
+            return data.level
+          })
         }
         break;
       case "opacity":
-        if (data.reset) {
-          setOpacityOfCurrentClip(100);
-        } else if (data.delta) {
-          changeOpacityOfCurrentClip(data.delta)
-        } else if (data.level) {
-          setOpacityOfCurrentClip(data.level)
+        if (castRelativeEvent(data)&&data.delta) {
+          modifyClip(new ModifyInfo(0,0),(info) => {
+            return info.getValue()+data.delta
+          })
+        } else if (castAbsoluteEvent(data)&&data.level) {
+          modifyClip(new ModifyInfo(0,0),() => {
+            return data.level
+          })
         }
         break;
       case "audio":
-        changeAudioLevel(data.delta)
+        if(castRelativeEvent(data)&&data.delta){
+          modifyClip(new ModifyInfo(0,0),(info) => {
+            return levelToDB(dbToLevel(parseFloat(info.getValue()))+data.delta)
+          })
+        }
         break;
       case "lumetri":
-        if (data.reset) {
-          resetLumetriProperty(data.property)
-        } else if (data.delta) {
-          changeLumetriProperty(data.delta, data.property, true)
-        } else if (data.level) {
-          changeLumetriProperty(data.level, data.property, false)
-        }
+
+        // TODO (1): Implement
+        // TODO (2): Create enum-like structure for easier property selection
         break;
     }
   }
 }
 
-function moveCurrentClip(deltaX: number, deltaY: number) {
-  const clipInfo = getFirstSelectedClip(true)
-  const positionInfo = clipInfo.clip.components[1].properties[0]
-  const [positionX, positionY] = positionInfo.getValue()
-  positionInfo.setValue([positionX + deltaX, positionY + deltaY], true)
+class ModifyInfo{
+  constructor(
+      public component:number,
+      public property:number,
+      public videoClip:boolean=true,
+      public setInfoBool:boolean=true
+  ) {
+  }
+
 }
 
-function resetPositionOfCurrentClip() {
-  const clipInfo = getFirstSelectedClip(true)
-  const positionInfo = clipInfo.clip.components[1].properties[0]
-  positionInfo.setValue([0.5, 0.5], true)
+/**
+ * Modify the clip according
+ * @param setting Which clip should get modified?
+ * @param processor How should that clip get modified?
+ */
+function modifyClip(setting:ModifyInfo, processor: (info:any)=>any){
+  const clipInfo = getFirstSelectedClip(setting.videoClip)
+  const info = clipInfo.clip.components[setting.component].properties[setting.property]
+  info.setValue(processor(info), setting.setInfoBool)
 }
 
-function setZoomOfCurrentClip(zoomLevel: number) {
-  const clipInfo = getFirstSelectedClip(true)
-  const scaleInfo = clipInfo.clip.components[1].properties[1]
-  scaleInfo.setValue(zoomLevel, true)
+function dbToLevel(db:number){
+  return 20 * Math.log(db) * Math.LOG10E + 15;
 }
 
-function changeZoomOfCurrentClip(delta: number) {
-  const clipInfo = getFirstSelectedClip(true)
-  const scaleInfo = clipInfo.clip.components[1].properties[1]
-  const current: number = scaleInfo.getValue()
-  scaleInfo.setValue(current + delta, true);
+function levelToDB(level:number){
+  return Math.min(Math.pow(10, (level - 15)/20), 1.0);
 }
 
-function setRotationOfCurrentClip(level: number) {
-  const clipInfo = getFirstSelectedClip(true)
-  const info = clipInfo.clip.components[1].properties[4]
-  info.setValue(level, true)
-}
-
-function rotateCurrentClip(delta: number) {
-  const clipInfo = getFirstSelectedClip(true)
-  const info = clipInfo.clip.components[1].properties[4]
-  const current: number = info.getValue()
-  info.setValue(current + delta, true)
-}
-
-function setOpacityOfCurrentClip(level: number) {
-  const clipInfo = getFirstSelectedClip(true)
-  const info = clipInfo.clip.components[0].properties[0]
-  info.setValue(level, true)
-}
-
-function changeOpacityOfCurrentClip(delta: number) {
-  const clipInfo = getFirstSelectedClip(true)
-  const info = clipInfo.clip.components[0].properties[0]
-  const current: number = info.getValue()
-  info.setValue(current + delta, true)
-}
-
-function changeAudioLevel(levelInDb: number) {
-  const clipInfo = getFirstSelectedClip(false)
-  const levelInfo = clipInfo.clip.components[0].properties[1];
-  const level = 20 * Math.log(parseFloat(levelInfo.getValue())) * Math.LOG10E + 15;
-  const newLevel = level + levelInDb;
-  const encodedLevel = Math.min(Math.pow(10, (newLevel - 15) / 20), 1.0);
-  levelInfo.setValue(encodedLevel, true);
-}
- 
 function getFirstSelectedClip(videoClip: boolean) {
   const currentSequence = app.project.activeSequence;
   const tracks = videoClip ? currentSequence.videoTracks : currentSequence.audioTracks;
@@ -146,52 +138,4 @@ function getFirstSelectedClip(videoClip: boolean) {
     }
   }
   return null;
-}
-
-/**
- * Changes the selected property of the current clip's first lumetri component.
- * @param data the relative or absolut value to set or add
- * @param propertyIndex the index of the property to change
- * @param relative true, if the change shall be relative
- */
-function changeLumetriProperty(data: number, propertyIndex: number, relative: boolean) {
-  const lumetri = getLumetriComponent()
-  if (lumetri) {
-    const property = lumetri.properties[propertyIndex]
-    if (relative) {
-      property.setValue(property.getValue() + data, true)
-    } else {
-      property.setValue(data, true)
-    }
-  }
-}
-
-/**
- * Resets the selected property of the current clip's first lumetri component.
- * @param propertyIndex 
- */
-function resetLumetriProperty(propertyIndex: number) {
-  // FIXME: Not all properties of lumetri are currently supported
-  const lumetri = getLumetriComponent()
-  if (lumetri) {
-    const property = lumetri.properties[propertyIndex]
-    if (propertyIndex == 24 || propertyIndex == 31 || propertyIndex == 36) {
-      property.setValue(100, true);
-    } else {
-      property.setValue(0, true);
-    }
-  }
-}
-
-/**
- * Returns the lumetri component of the currently selected clip or undefined.
- */
-function getLumetriComponent(): any {
-  const clip = getFirstSelectedClip(true).clip
-  for (let i = 2; i < clip.components.length; i++) {
-    if (clip.components[i].properties[2].displayName && clip.components[i].properties[2].displayName === "Einfache Korrektur") {
-      return clip.components[i]
-    }
-  }
-  return undefined
 }
